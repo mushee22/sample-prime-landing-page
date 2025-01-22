6092167a89d2c739d10998ee1925cb8b85a0471e
Greeshma G <greeshma@pixbitsolutions.com>
2025-01-22 11:44:05 +0530

    feat(home): update service section

--- src/components/elements/Home/ServiceCard.tsx
+++ src/components/elements/Home/ServiceCard.tsx
@@ -14,8 +14,8 @@ interface ServiceCardProps {
 function ServiceCard({ icon, title, className }: ServiceCardProps) {
     return (
         <Card className={cn('relative group rounded-3xl overflow-hidden border-none shadow-none cursor-pointer h-full', className)}>
-            <motion.div className="absolute inset-0 m-auto w-20 h-20 bg-primary scale-1 group-hover:scale-[8] opacity-0 group-hover:opacity-100 rounded-full transition-transform duration-700"/>
-            <CardContent className='p-6 md:p-10 xl:p-12 relative h-full grid content-between'>
+            <motion.div className="absolute inset-0 m-auto w-20 h-20 bg-primary scale-1 group-hover:scale-[8] opacity-0 group-hover:opacity-100 rounded-full ease-in-out duration-700"/>
+            <CardContent className='p-6 md:py-12 relative h-full grid content-between'>
                 <Image src={icon} alt={title} className='mx-auto size-14 group-hover:rotate-[120deg] ease-linear duration-300' />
                 <Typography variant='title' size='md' className='text-dark text-center'> {title} </Typography>
             </CardContent>
@@ -23,4 +23,4 @@ function ServiceCard({ icon, title, className }: ServiceCardProps) {
     )
 }
 
-export default ServiceCard
+export default ServiceCard   
--- src/components/sections/Index/Service.tsx
+++ src/components/sections/Index/Service.tsx
@@ -7,18 +7,19 @@ import i2 from '@/app/assets/vectors/i2.png'
 import i3 from '@/app/assets/vectors/i3.png'
 import i4 from '@/app/assets/vectors/i4.png'
 import i5 from '@/app/assets/vectors/i5.png'
+import i6 from '@/app/assets/vectors/i6.png'
 
 const Service = () => {
     return (
-        <section className="bg-secondary pt-16 md:pt-26 pb-26 rounded-b-40" id="services">
-            <Container>
+        <section className="bg-secondary pt-16 md:pt-26 pb-26 md:rounded-b-40" id="services">
+            <Container className="max-w-screen-2xl">
                 <div className="text-center mb-14">
                     <Typography variant='heading' size='md' className='text-dark mb-6'>Our Services</Typography>
                     <Typography variant='title' size='sm' className='text-dark md:max-w-3xl mx-auto'>type of partner for today&apos;s clients. We bring our collective expertise, AI-driven intelligence and cut-through creativity to counsel clients and deliver transformative ideas that shape culture, spark change and deliver impact.</Typography>
                 </div>
                 <div className="flex flex-wrap gap-2 md:gap-4 justify-center">
                     {services.map((service, i) => (
-                        <div key={i} className="min-h-56 md:min-h-80 basis-[calc(50%-.5rem)] max-md:last:basis-full lg:basis-[calc(25%-1rem)] 2xl:basis-[calc(20%-1rem)] max-w-[300px] min-w-[132px] md:min-w-[240px] max-md:last:max-w-[370px] max-md:last:min-w-[132px]">
+                        <div key={i} className="min-h-56 md:min-h-80 basis-[calc(50%-.5rem)] xl:basis-[calc(25%-1rem)] max-w-[340px] min-w-[132px] md:min-w-[300px] md:max-w-[340px]">
                             <ServiceCard {...service} />
                         </div>
                     ))}
@@ -34,7 +35,7 @@ export default Service
 const services = [
     {
         icon: i1,
-        title: "PR & Events"
+        title: "PR"
     },
     {
         icon: i2,
@@ -42,14 +43,18 @@ const services = [
     },
     {
         icon: i3,
-        title: "Digital & SEO"
+        title: "Digital & CRM"
     },
     {
         icon: i4,
-        title: "Design & Branding"
+        title: "Brand Design"
     },
     {
         icon: i5,
         title: "Influencer Marketing",
     },
+    {
+        icon: i6,
+        title: "Events & Activations "
+    }
 ]
