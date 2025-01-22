be44e2495addab8f7e86570e6129523acc3f3db2
Greeshma G <greeshma@pixbitsolutions.com>
2025-01-22 19:36:59 +0530

    fix: remove @studio-freight/react-lenis replace with lenis

--- src/components/ui/SmoothScrolling.tsx
+++ src/components/ui/SmoothScrolling.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { ReactNode } from "react";
-import { ReactLenis } from "@studio-freight/react-lenis";
+// import { ReactLenis } from "@studio-freight/react-lenis";
+import { ReactLenis } from 'lenis/react'
 
 interface Props {
   children?: ReactNode;
