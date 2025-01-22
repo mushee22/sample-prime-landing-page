29b4a4a16ccd17d0e906e460d0e4307ed1dce1b1
Greeshma G <greeshma@pixbitsolutions.com>
2025-01-22 14:24:58 +0530

    feat(home): add video

--- src/components/sections/Index/HeroVideo.tsx
+++ src/components/sections/Index/HeroVideo.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image"
 import Container from "@/components/ui/Container"
 
-import hero from '@/app/assets/home/i.png'
 
 import { motion } from 'framer-motion'
 
 const HeroVideo = () => {
     return (
-        <section className="h-[98vh] md:h-screen pt-24">
-            <Container className="h-full">
+        <section className="h-screen">
+            <Container className="h-full !p-4">
                 <motion.div
                     initial={{ opacity: 0.9, scale: 0.5, y: 320 }}
                     animate={{ opacity: 1, scale: 1, y: 0 }}
@@ -19,9 +18,18 @@ const HeroVideo = () => {
                         stiffness: 90,
                         mass: 1
                     }}
-                    className="rounded-40 overflow-hidden h-full"
+                    className="rounded-3xl md:rounded-40 overflow-hidden h-full"
                 >
-                    <Image src={hero} alt="" className="w-full h-full" />
+                    {/* <Image src={hero} alt="" className="w-full h-full" /> */}
+                    <video src="/hero-video.mp4"
+                        autoPlay
+                        muted
+                        loop
+                        controlsList="nodownload"
+                        playsInline
+                        webkit-playsinline="true"
+                        width="100%"
+                        height="100%" className="object-cover h-full w-full aspect-video"></video>
                 </motion.div>
             </Container>
         </section>
