90461f187af3352485a5a376aff21cd4478ee138
mushee22 <mmtmusheer@gmail.com>
2026-01-17 11:56:57 +0530

    style: Update hamburger menu icon color from orange to blue.

--- src/components/sections/Header/HamburgerMenu.tsx
+++ src/components/sections/Header/HamburgerMenu.tsx
@@ -9,7 +9,7 @@ interface HamburgerMenuProps {
 const HamburgerMenu: React.FC<HamburgerMenuProps> = ({ isOpen, setOpen }) => {
     return (
         <div className='border border-primary/10 rounded-xl flex items-center justify-center w-14 h-14'>
-            <Hamburger toggled={isOpen} toggle={setOpen} color='#F3951D' direction='right' />
+            <Hamburger toggled={isOpen} toggle={setOpen} color='#1f70b2' direction='right' />
         </div>
     );
 };
